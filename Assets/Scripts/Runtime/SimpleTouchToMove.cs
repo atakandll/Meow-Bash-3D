c08bb4067f3e626b640cf659a9f98ef6e3af69de
atakandll <atakanccnar@outlook.com>
2023-12-18 13:59:53 +0300

    Cat move and jump movements finished

--- Assets/Scripts/Runtime/SimpleTouchToMove.cs
+++ Assets/Scripts/Runtime/SimpleTouchToMove.cs
@@ -1,3 +1,4 @@
+using UnityEditor.UIElements;
 using UnityEngine;
 
 namespace Runtime
@@ -9,9 +10,10 @@ namespace Runtime
         #region Public Variables
         
         public CharacterController CharacterController;
-        private Vector3 _moveDirection;
         public float Speed = 6.0f;
-        public float gravity = -9.8f;
+        public float gravity = 10f;
+        public float JumpForce = 3.0f;
+        public float StopForce = 3.0f;
         
         #endregion
 
@@ -20,6 +22,9 @@ namespace Runtime
         private Touch _touch;
         private Vector2 _initPos;
         private Vector2 _direction;
+        private Vector3 _moveDirection;
+
+        
         
         private bool canMove;
         
@@ -28,17 +33,45 @@ namespace Runtime
         #endregion
        
 
-        // Update is called once per frame
         void Update()
         {
             if (Input.touchCount > 0)
             {
                 canMove = true;
+                _touch = Input.GetTouch(0);
+                
+                if (_touch.phase == TouchPhase.Began)
+                {
+                    _initPos = _touch.position;
+                }
+
+                if (_touch.phase == TouchPhase.Moved)
+                {
+                    _direction = _touch.deltaPosition;
+                }
+
+                if (CharacterController.isGrounded)
+                {
+                    _moveDirection = new Vector3(_touch.position.x - _initPos.x, 0, _touch.position.y - _initPos.y);
+                    Quaternion targetRotation = _moveDirection != Vector3.zero ? Quaternion.LookRotation(_moveDirection) : transform.rotation;
+                    transform.rotation = targetRotation;
+                    _moveDirection = _moveDirection * Speed;
+                }
             }
             else
             {
                 canMove = false;
+                _moveDirection = Vector3.Lerp(_moveDirection, Vector3.zero, Time.deltaTime * StopForce);
             }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _moveDirection.y += JumpForce;
+            }
+            _moveDirection.y -= (gravity * Time.deltaTime);
+            CharacterController.Move(_moveDirection * Time.deltaTime);
+
+
         }
     }
 }
