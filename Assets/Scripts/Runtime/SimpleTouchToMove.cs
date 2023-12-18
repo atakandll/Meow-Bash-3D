ef9e100ecc50c28b8d39db7f3820b3de7c0791c6
atakandll <atakanccnar@outlook.com>
2023-12-18 15:08:11 +0300

    Player movement and animations finished

--- Assets/Scripts/Runtime/SimpleTouchToMove.cs
+++ Assets/Scripts/Runtime/SimpleTouchToMove.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Runtime
 {
@@ -9,7 +11,6 @@ namespace Runtime
 
         #region Public Variables
         
-        public CharacterController CharacterController;
         public float Speed = 6.0f;
         public float gravity = 10f;
         public float JumpForce = 3.0f;
@@ -18,20 +19,26 @@ namespace Runtime
         #endregion
 
         #region Private Variables
+        
+        private Animator _animator;
+        private CharacterController _characterController;
 
         private Touch _touch;
         private Vector2 _initPos;
         private Vector2 _direction;
         private Vector3 _moveDirection;
-
-        
-        
         private bool canMove;
         
         #endregion
 
         #endregion
-       
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _characterController = GetComponent<CharacterController>();
+        }
+
 
         void Update()
         {
@@ -50,7 +57,7 @@ namespace Runtime
                     _direction = _touch.deltaPosition;
                 }
 
-                if (CharacterController.isGrounded)
+                if (_characterController.isGrounded)
                 {
                     _moveDirection = new Vector3(_touch.position.x - _initPos.x, 0, _touch.position.y - _initPos.y);
                     Quaternion targetRotation = _moveDirection != Vector3.zero ? Quaternion.LookRotation(_moveDirection) : transform.rotation;
@@ -63,13 +70,14 @@ namespace Runtime
                 canMove = false;
                 _moveDirection = Vector3.Lerp(_moveDirection, Vector3.zero, Time.deltaTime * StopForce);
             }
+            _animator.SetBool("canWalk", canMove);
 
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0) && _characterController.isGrounded)
             {
                 _moveDirection.y += JumpForce;
             }
             _moveDirection.y -= (gravity * Time.deltaTime);
-            CharacterController.Move(_moveDirection * Time.deltaTime);
+            _characterController.Move(_moveDirection * Time.deltaTime);
 
 
         }
