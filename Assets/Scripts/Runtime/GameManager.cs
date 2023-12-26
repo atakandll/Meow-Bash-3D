859e93e7068a4a6a18d900d20b53f4958f6c036d
atakandll <atakanccnar@outlook.com>
2023-12-26 09:26:52 +0300

    Setting up timer

--- Assets/Scripts/Runtime/GameManager.cs
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Runtime
@@ -8,10 +9,15 @@ namespace Runtime
     public class GameManager : MonoBehaviour
     {
         public bool GameStartarted = false;
+        public  bool GameEnded = false;
 
         public int Level = 1;
         public GameObject[] rooms;
         public GameObject[] tutorials;
+        public GameObject endScreen;
+        [SerializeField] private int timerValue;
+        [SerializeField] private TextMeshProUGUI textScore;
+        
 
         private void Awake()
         {
@@ -28,7 +34,27 @@ namespace Runtime
             {
                 GameStartarted = true;
                 tutorials[0].SetActive(false);
+                InvokeRepeating("SetTimer",1,1);
             }
+        }
+
+        public void SetTimer()
+        {
+            timerValue--;
+            textScore.text = timerValue.ToString();
+            
+            if (timerValue == 0)
+            {
+                GameEnded = true;
+                CancelInvoke();
+                endScreen.SetActive(true);
+            }
+        }
+
+        public void RestartGame()
+        {
+            timerValue = 20;
+            Application.LoadLevel(Application.loadedLevelName);
         }
     }
 }
--- Assets/Scripts/Runtime/SimpleTouchToMove.cs
+++ Assets/Scripts/Runtime/SimpleTouchToMove.cs
@@ -8,7 +8,8 @@ namespace Runtime
         #region Self Variables
 
         #region Public Variables
-        
+
+        public GameManager gameManager;
         public float Speed;
         public float gravity = 10f;
         public float JumpForce;
@@ -44,45 +45,48 @@ namespace Runtime
 
         void Update()
         {
-            if (Input.touchCount > 0)
+            if (!gameManager.GameEnded && gameManager.GameStartarted)
             {
-                canMove = true;
-                _touch = Input.GetTouch(0);
+                if (Input.touchCount > 0)
+                {
+                    canMove = true;
+                    _touch = Input.GetTouch(0);
                 
-                if (_touch.phase == TouchPhase.Began)
-                {
-                    _initPos = _touch.position;
+                    if (_touch.phase == TouchPhase.Began)
+                    {
+                        _initPos = _touch.position;
+                    }
+
+                    if (_touch.phase == TouchPhase.Moved)
+                    {
+                        _direction = _touch.deltaPosition;
+                    }
+
+                    if (_characterController.isGrounded)
+                    {
+                        _moveDirection = new Vector3(_touch.position.x - _initPos.x, 0, _touch.position.y - _initPos.y);
+                        Quaternion targetRotation = _moveDirection != Vector3.zero ? Quaternion.LookRotation(_moveDirection) : transform.rotation;
+                        transform.rotation = targetRotation;
+                        _moveDirection = _moveDirection.normalized * Speed;
+                    }
                 }
-
-                if (_touch.phase == TouchPhase.Moved)
+                else
                 {
-                    _direction = _touch.deltaPosition;
+                    canMove = false;
+                    _moveDirection = Vector3.Lerp(_moveDirection, Vector3.zero, Time.deltaTime * StopForce);
                 }
+                _animator.SetBool("canWalk", canMove);
 
-                if (_characterController.isGrounded)
+                if (Input.GetMouseButtonUp(0) && _characterController.isGrounded)
                 {
-                    _moveDirection = new Vector3(_touch.position.x - _initPos.x, 0, _touch.position.y - _initPos.y);
-                    Quaternion targetRotation = _moveDirection != Vector3.zero ? Quaternion.LookRotation(_moveDirection) : transform.rotation;
-                    transform.rotation = targetRotation;
-                    _moveDirection = _moveDirection.normalized * Speed;
+                    Instantiate(jumpEffect, transform.position, Quaternion.identity);
+                    _moveDirection.y += JumpForce;
                 }
+                _moveDirection.y -= (gravity * Time.deltaTime);
+            
+                _characterController.Move(_moveDirection * Time.deltaTime);
             }
-            else
-            {
-                canMove = false;
-                _moveDirection = Vector3.Lerp(_moveDirection, Vector3.zero, Time.deltaTime * StopForce);
-            }
-            _animator.SetBool("canWalk", canMove);
-
-            if (Input.GetMouseButtonUp(0) && _characterController.isGrounded)
-            {
-                Instantiate(jumpEffect, transform.position, Quaternion.identity);
-                _moveDirection.y += JumpForce;
-            }
-            _moveDirection.y -= (gravity * Time.deltaTime);
-            _characterController.Move(_moveDirection * Time.deltaTime);
-
-
+            
         }
     }
 }
