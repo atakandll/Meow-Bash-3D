ff47a13518b962f8b67be3ae4edda487f84c62ea
atakandll <atakanccnar@outlook.com>
2024-01-03 07:29:22 +0300

    Detection of collisions with objects

---
+++ Assets/Scripts/Runtime/CatHitter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Runtime
+{
+    public class CatHitter : MonoBehaviour
+    {
+        public GameObject[] hitParticles;
+        
+        private Rigidbody _rigidbody;
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            if (hit.gameObject.CompareTag("hittable"))
+            {
+                Debug.Log("object hitted" + hit.gameObject.name);
+                hit.gameObject.tag = "hitted";
+                _rigidbody = hit.gameObject.GetComponent<Rigidbody>();
+                _rigidbody.isKinematic = false;
+                _rigidbody.AddExplosionForce(75,transform.position + Vector3.down,15);
+                Instantiate(hitParticles[Random.Range(0, hitParticles.Length)], hit.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
