93188b50871e4aecd03219b3afd1dfcd8badfa65
atakandll <atakanccnar@outlook.com>
2024-01-29 21:49:48 +0300

    Levels added and more AI player added

--- Assets/Scripts/Runtime/RandomNameGenerator.cs
+++ Assets/Scripts/Runtime/RandomNameGenerator.cs
@@ -6,7 +6,7 @@ namespace Runtime
 {
     public class RandomNameGenerator : MonoBehaviour
     {
-        string[] names = {"Ksusha", "Boncuk", "Maestro", "Zoltan", "Wilco", "Uri","Oslo", "Bubu", "Gogol","Fred", "Tolik","Frank", "George","Ava","Betty","Cindy","Diana","Elena","Fiona","Gina","Hanna","Ivan","Jana"};
+        string[] names = {"Ksusha", "Boncuk", "Pamuk", "Zoltan", "Wilco", "Uri","Oslo", "Bubu", "Gogol","Fred"};
 
         private void Awake()
         {
